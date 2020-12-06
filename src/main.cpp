98927d28b57bea3732843330e970138fd496dbc3
3rdCore <tom.marty@polytechnique.edu>
2020-12-06 15:26:55 +0100

    linked opencv lib and wrote a main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Histogram.hpp"
+#include "BagOfFeatures.h"
+
+using namespace std;
+
+
+
+Vocabulary Histogram::vocabulary=Vocabulary();
+
+int main()
+{
+
+
+	return 0;
+}
