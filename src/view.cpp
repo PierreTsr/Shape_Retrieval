9ef630a40edd7b6b4f1e3c714c177dbb6bb6d2bf
3rdCore <tom.marty@polytechnique.edu>
2020-12-09 18:49:53 +0100

    created view class

---
+++ src/view.cpp
@@ -0,0 +1,12 @@
+//
+// Created by tom on 09/12/2020.
+//
+
+#include "view.h"
+
+
+view::view(Mat& _image,Vocabulary& _vocab):vocab(_vocab), Image(_image){
+
+}
+void CreateBoF();
+void CreateHisto();
